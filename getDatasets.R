0ba79948bbbd8f20b7256a12fbb8b9e54b071c28
Joel Boersma <joelboersma@gmail.com>
2020-01-07 16:42:33 -0800

    added mlb dataset

--- getDatasets.R
+++ getDatasets.R
@@ -1,4 +1,5 @@
 tg <- ToothGrowth
 pima <- read.csv('http://heather.cs.ucdavis.edu/FasteR/data/Pima.csv', header=TRUE)
 pe <- read.table('https://raw.githubusercontent.com/matloff/polyreg/master/data/prgeng.txt', header=TRUE)
+mlb <- read.table('https://raw.githubusercontent.com/matloff/fasteR/master/data/mlb.txt',header=TRUE)
 print("Done!")
