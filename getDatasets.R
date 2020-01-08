db8ebe2a484d0b6e5910107dfcfe0f6691606b29
Joel Boersma <joelboersma@gmail.com>
2020-01-07 16:12:47 -0800

    getDatasets

---
+++ getDatasets.R
@@ -0,0 +1,4 @@
+tg <- ToothGrowth
+pima <- read.csv('http://heather.cs.ucdavis.edu/FasteR/data/Pima.csv', header=TRUE)
+pe <- read.table('https://raw.githubusercontent.com/matloff/polyreg/master/data/prgeng.txt', header=TRUE)
+print("Done!")
