6edc8a604ec36719327a30b258f9bd7126d20ba1
Joel Boersma <joelboersma@gmail.com>
2020-01-09 14:28:18 -0800

    added abt to getDatasets

--- getDatasets.R
+++ getDatasets.R
@@ -2,4 +2,5 @@ tg <- ToothGrowth
 pima <- read.csv('http://heather.cs.ucdavis.edu/FasteR/data/Pima.csv', header=TRUE)
 pe <- read.table('https://raw.githubusercontent.com/matloff/polyreg/master/data/prgeng.txt', header=TRUE)
 mlb <- read.table('https://raw.githubusercontent.com/matloff/fasteR/master/data/mlb.txt',header=TRUE)
+abt <- readLines('https://raw.githubusercontent.com/matloff/fasteR/master/data/aboutR.txt')
 print("Done!")
